78d214be21720e3e7a825a4f566a8669d4ac1dcf
Mogomotsi Moroane <mmoroane@hotmail.com>
2023-01-10 22:14:41 +0200

    Added annotation for authenticate endpoint

--- src/main/java/com/simple/jwt/controller/JwtAuthenticationController.java
+++ src/main/java/com/simple/jwt/controller/JwtAuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,7 @@ public class JwtAuthenticationController {
         }
     }
 
+    @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthentication(@RequestBody JwtRequest authenticationRequest) throws Exception {
         authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
