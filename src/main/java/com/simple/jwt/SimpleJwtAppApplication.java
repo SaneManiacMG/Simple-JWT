24c342e3cc10b31995e982bf3524f75277ba7f41
Mogomotsi Moroane <mmoroane@hotmail.com>
2023-01-10 19:35:11 +0200

    Init Commit

---
+++ src/main/java/com/simple/jwt/SimpleJwtAppApplication.java
@@ -0,0 +1,13 @@
+package com.simple.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJwtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJwtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple/jwt/SimpleJwtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJwtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
