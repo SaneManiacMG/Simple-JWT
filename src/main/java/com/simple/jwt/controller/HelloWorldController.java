4b599048c04ea248031f0cc4288077890858f20e
Mogomotsi Moroane <mmoroane@hotmail.com>
2023-01-10 19:37:08 +0200

    Created HelloWorldController

---
+++ src/main/java/com/simple/jwt/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.simple.jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String firstPage() {
+        return "Hello World!";
+    }
+}
